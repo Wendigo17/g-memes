7a0e41bcac12dfd4eeed6acaeb479919449e1bbb
Wendigo17 <58408808+Wendigo17@users.noreply.github.com>
2020-01-13 13:49:16 -0500

    Add files via upload

---
+++ C++/Leaker -sanic/Leaker.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+	int x{ 1 };
+	while (x == 1)
+	{
+		unsigned long long int *peepee = new unsigned long long int;
+		unsigned long long int *weewee = new unsigned long long int;
+		unsigned long long int *h = new unsigned long long int;
+	}
+}
