998fd5042c3e270342ec7a284cc51fca0b5aabbe
Wendigo17 <58408808+Wendigo17@users.noreply.github.com>
2020-01-13 13:54:12 -0500

    Update Leaker.cpp

--- C++/Leaker -sanic/Leaker.cpp
+++ C++/Leaker -sanic/Leaker.cpp
@@ -3,8 +3,8 @@ int main()
 	int x{ 1 };
 	while (x == 1)
 	{
-		unsigned long long int *peepee = new unsigned long long int;
-		unsigned long long int *weewee = new unsigned long long int;
+		unsigned long long int *whyarewehere = new unsigned long long int;
+		unsigned long long int *justtosuffer = new unsigned long long int;
 		unsigned long long int *h = new unsigned long long int;
 	}
 }
